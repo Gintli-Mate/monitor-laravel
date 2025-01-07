7a298da4dd064a0b71f44c552a6baa9f572a3fc7
Gintli-Mate <gintli.mate@students.jedlik.eu>
2025-01-07 10:39:33 +0100

    first commit

---
+++ db.sql
@@ -0,0 +1,12 @@
+INSERT INTO monitors (id, brand, model, screen_size, resolution, refresh_rate, price, type, availability) 
+VALUES
+(1, 'Samsung', 'Odyssey G7', 27, '2560x1440', 240, 599.99, 'Curved', 1),
+(2, 'LG', 'UltraGear 27GN950', 27, '3840x2160', 144, 799.99, 'Flat', 1),
+(3, 'Dell', 'Alienware AW2521H', 24.5, '1920x1080', 360, 799.99, 'Flat', 1),
+(4, 'Acer', 'Predator X27', 27, '3840x2160', 144, 1499.99, 'Flat', 0),
+(5, 'BenQ', 'Zowie XL2546K', 24.5, '1920x1080', 240, 429.99, 'Flat', 1),
+(6, 'ASUS', 'ROG Swift PG259QN', 24.5, '1920x1080', 360, 899.99, 'Flat', 1),
+(7, 'MSI', 'Optix MAG272CQR', 27, '2560x1440', 165, 349.99, 'Curved', 1),
+(8, 'Gigabyte', 'G32QC', 32, '2560x1440', 165, 299.99, 'Curved', 0),
+(9, 'ViewSonic', 'XG2705-2K', 27, '2560x1440', 165, 319.99, 'Flat', 1),
+(10, 'Philips', 'Momentum 279M1RV', 27, '3840x2160', 60, 599.99, 'Flat', 0);
