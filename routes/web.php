ecdeae9a4caa2cbfcc836f88c2cd9bb1ffbc47ba
Gintli-Mate <gintli.mate@students.jedlik.eu>
2025-01-07 11:45:38 +0100

    végleges

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
 use App\Http\Controllers\MonitorController;
+use Illuminate\Support\Facades\Route;
 
+
+Route::redirect('/', '/monitors');
 Route::resource('monitors', MonitorController::class);
